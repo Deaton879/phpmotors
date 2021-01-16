db96d4434be71f552312c94ea94a71a1f741c8f9
DALLAS EATON <deaton879@byui.edu>
2021-01-16 03:14:04 -0500

    Finished Enhancement 1

---
+++ modules/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home | PHP Motors</title>
+    <link href="/phpmotors/css/styles.css" type="text/css" rel="stylesheet" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <div id="top-header">
+                <img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo">
+                <a href="/phpmotors/accounts?action=login-page" title="Login or Register with PHP Motors" id="acc">My Account</a>
+            </div>
+        </header>
+        <nav>
+            <?php include$_SERVER['DOCUMENT_ROOT'] . '/phpmotors/modules/nav.php'; ?>
+        </nav>
+        <main>
---
+++ template.php
@@ -0,0 +1,5 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/modules/header.php'; ?>
+
+<h1>Content Title Here</h1>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/modules/footer.php'; ?>
