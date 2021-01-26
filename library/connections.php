3858895d625e7aed827e4ecfe0097448b7b10048
DALLAS EATON <deaton879@byui.edu>
2021-01-26 02:36:06 -0500

    Week3 Finished

---
+++ library/connections.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Proxy connection to PHPmotors database
+*/
+
+function phpmotorsConnect() {
+
+    $server = 'mysql';
+    $dbname = 'phpmotors';
+    $username = 'iClient';
+    $password = 'TZKwF2RyWhW4nfUu';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $link = new PDO($dsn, $username, $password, $option);
+        return $link;
+        //echo 'It worked!';
+    } catch(PDOException $e) {
+        header('Location: /phpmotors/view/500.php');
+        exit;
+        //echo "It didn't work, error: " . $e->getMessage();
+    }
+    
+}
+phpmotorsConnect();
+
+?>
---
+++ view/500.php
@@ -0,0 +1,6 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/modules/header.php'; ?>
+<div id="error">
+    <h1>Server Error</h1>
+    <p>Sorry our server seems to be experiencing some technical difficulties. Please check back later.</p>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/modules/footer.php'; ?>
